41514e45fa3cbe269686131654b521f197df6676
rizaton <123567427+rizaton@users.noreply.github.com>
2024-12-31 16:21:10 +0700

    feat(product): form

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,6 @@ class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        URL::forceScheme('https');
         return $panel
             ->default()
             ->id('admin')
